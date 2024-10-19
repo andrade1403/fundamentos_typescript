cab4d284bfb9b8bc1d2b1ab68b911cc8537f7976
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-10-19 15:16:15 -0500

    Commit final.

--- main.ts
+++ main.ts
@@ -29,7 +29,7 @@ mostrarCursosAprendiz(ap.cursos);
 
 function mostrarDatosAprendiz(aprendiz: Aprendiz): void{
     let tbodyAprendiz = document.createElement('tbody');
-    tbodyAprendiz.innerHTML = `<tr><td colspan = 2 ><img src = './${aprendiz.avatar} height = '100'></tr>
+    tbodyAprendiz.innerHTML = `<tr><td colspan = 2 ><img src = './${aprendiz.avatar} height = '100'></td></tr>
     <tr><td>Nombres:</td><td>${aprendiz.nombres}</td></tr>
     <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></tr>
     <tr><td>Nivel educativo:</td><td>${aprendiz.nivelEducativo}</td></tr>
--- scripts/main.js
+++ scripts/main.js
@@ -23,7 +23,7 @@ mostrarEstadisticas(ap);
 mostrarCursosAprendiz(ap.cursos);
 function mostrarDatosAprendiz(aprendiz) {
     var tbodyAprendiz = document.createElement('tbody');
-    tbodyAprendiz.innerHTML = "<tr><td colspan = 2 ><img src = './".concat(aprendiz.avatar, " height = '100'></tr>\n    <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n    <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n    <tr><td>Nivel educativo:</td><td>").concat(aprendiz.nivelEducativo, "</td></tr>\n    <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
+    tbodyAprendiz.innerHTML = "<tr><td colspan = 2 ><img src = './".concat(aprendiz.avatar, " height = '100'></td></tr>\n    <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n    <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n    <tr><td>Nivel educativo:</td><td>").concat(aprendiz.nivelEducativo, "</td></tr>\n    <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
     aprendizTable.appendChild(tbodyAprendiz);
 }
 function mostrarEstadisticas(aprendiz) {
